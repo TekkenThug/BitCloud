6c26247d8d344873c745a11b2d36f3e87fa23e2f
Vadim Ignatov <VSIGNATOV1@rolftech.ru>
2023-09-21 20:01:44 +0300

    feat: create market header block

---
+++ src/components/pages/market/header/MarketHeader.tsx
@@ -0,0 +1,28 @@
+import styles from "./MarketHeader.module.scss";
+import image from "assets/images/market/header.png";
+
+const MarketHeader = () => {
+	const text = "Today’s Cryptocurrency prices";
+	const caption = "The global crypto market cap is";
+	const cryptoMarketCup = "$1.86T";
+
+	return (
+		<div className={styles.MarketHeader}>
+			<div className={styles.content}>
+				<h1 className={styles.title}>
+					{ text }
+				</h1>
+
+				<p className={styles.caption}>
+					{ caption } <b>{ cryptoMarketCup }</b>
+				</p>
+			</div>
+
+			<div className={styles.imagePlaceholder}>
+				<img src={image} alt="Market Placeholder" />
+			</div>
+		</div>
+	);
+};
+
+export default MarketHeader;
