3b67851431545ebfaff4bfc27bfbc11c853a150e
Vadim Ignatov <ignatov0131@gmail.com>
2023-09-23 15:34:04 +0300

    feat: make market table

---
+++ src/components/pages/market/table/row/MarketTableRow.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+import { CurrencyRow } from "types/currency";
+
+import ChartSmall from "components/common/charts/ChartSmall";
+
+import styles from "./MarketTableRow.module.scss";
+import { ReactComponent as Star } from "assets/icons/ui/star.svg";
+import { ReactComponent as Bitcoin } from "assets/icons/currency/bitcoin.svg";
+import { ReactComponent as Eth } from "assets/icons/currency/eth.svg";
+import { ReactComponent as USDT } from "assets/icons/currency/usdt.svg";
+
+const MarketTableRow: React.FC<CurrencyRow> = (
+	{
+		number,
+		fullName,
+		shortName,
+		price,
+		percentageForWeek,
+		percentageForDay,
+		capitalization,
+		volume
+	}
+) => {
+	const chartColor = percentageForDay < 0 ? "#FF6838" : "#58BD7D";
+
+	const renderCurrencyIcon = () => {
+		if (shortName === "BTC") {
+			return <Bitcoin className={styles.cellCurrencyIcon} />;
+		}
+
+		if (shortName === "ETH") {
+			return <Eth className={styles.cellCurrencyIcon} />;
+		}
+
+		if (shortName === "USDT") {
+			return <USDT className={styles.cellCurrencyIcon} />;
+		}
+	};
+
+	return (
+		<div className={styles.MarketTableRow}>
+			<div className={styles.cell}>
+				<Star className={styles.cellStar} /> { number }
+			</div>
+
+			<div className={styles.cell}>
+				{ renderCurrencyIcon() } { fullName } <span className={styles.shortName}>{ shortName }</span>
+			</div>
+
+			<div className={styles.cell}>
+				${ price.toLocaleString("en-US", { minimumFractionDigits: 2 }) }
+			</div>
+
+			<div className={`${styles.cell} ${percentageForWeek > 0 ? styles.cell_positive : styles.cell_negative }`}>
+				{ percentageForWeek > 0 ? "+" : "" }{ percentageForWeek }%
+			</div>
+
+			<div className={`${styles.cell} ${percentageForDay > 0 ? styles.cell_positive : styles.cell_negative }`}>
+				{ percentageForDay > 0 ? "+" : "" }{ percentageForDay }%
+			</div>
+
+			<div className={styles.cell}>
+				${ capitalization.toLocaleString("en-US", { minimumFractionDigits: 0 }) }
+			</div>
+
+			<div className={styles.cell}>
+				${ volume.toLocaleString("en-US", { minimumFractionDigits: 0 }) }
+			</div>
+
+			<div className={styles.cell}>
+				<ChartSmall
+					color={chartColor}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default MarketTableRow;
---
+++ src/types/currency.ts
@@ -0,0 +1,10 @@
+export interface CurrencyRow {
+	number: number,
+	fullName: string;
+	shortName: string;
+	price: number;
+	percentageForDay: number;
+	percentageForWeek: number;
+	capitalization: number;
+	volume: number;
+}
