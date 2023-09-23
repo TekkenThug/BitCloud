5393ac30b4a7eeff64a64a1eb57934c993ebe527
Vadim Ignatov <ignatov0131@gmail.com>
2023-09-23 20:35:13 +0300

    feat: add animation for heading

--- src/components/pages/market/header/MarketHeader.tsx
+++ src/components/pages/market/header/MarketHeader.tsx
@@ -1,7 +1,14 @@
 import MarketWidget from "components/pages/market/widget/MarketWidget";
 
 import styles from "./MarketHeader.module.scss";
-import image from "assets/images/market/header.png";
+import image from "assets/images/market/header/layout.png";
+import cursor from "assets/images/market/header/cursor.png";
+import elementOne from "assets/images/market/header/element-1.png";
+import elementTwo from "assets/images/market/header/element-2.png";
+import elementThree from "assets/images/market/header/element-3.png";
+import elementFour from "assets/images/market/header/element-4.png";
+import elementFive from "assets/images/market/header/element-5.png";
+import { MouseEventHandler, useEffect } from "react";
 
 const MarketHeader = () => {
 	const text = "Today’s Cryptocurrency prices";
@@ -24,9 +31,46 @@ const MarketHeader = () => {
 
 					<div className={styles.imagePlaceholder}>
 						<img src={image} alt="Market Placeholder" />
+
+						<img
+							className={`${styles.imageDecorator} ${styles.cursor}`}
+							src={cursor}
+							alt="Cursor"
+						/>
+
+						<img
+							className={`${styles.imageDecorator} ${styles.elementOne}`}
+							src={elementOne}
+							alt="Element one"
+						/>
+
+						<img
+							className={`${styles.imageDecorator} ${styles.elementTwo}`}
+							src={elementTwo}
+							alt="Element two"
+						/>
+
+						<img
+							className={`${styles.imageDecorator} ${styles.elementThree}`}
+							src={elementThree}
+							alt="Element three"
+						/>
+
+						<img
+							className={`${styles.imageDecorator} ${styles.elementFour}`}
+							src={elementFour}
+							alt="Element four"
+						/>
+
+						<img
+							className={`${styles.imageDecorator} ${styles.elementFive}`}
+							src={elementFive}
+							alt="Element five"
+						/>
 					</div>
 				</div>
 			</div>
+
 			<div className={`${styles.widget} container`}>
 				<MarketWidget />
 			</div>
