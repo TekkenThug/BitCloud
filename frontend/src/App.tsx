789905f1d8f038d4eb9f70911e9f1ec6703f8ad2
Vadim Ignatov <ignatov0131@gmail.com>
2023-09-23 22:15:51 +0300

    feat: replace core files to vite project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "components/common/header/Header";
+import Market from "components/pages/market/Market";
+import Footer from "components/common/footer/Footer";
 
-function App() {
-  const [count, setCount] = useState(0)
+import "assets/styles/global.scss";
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => {
+	return (
+		<div>
+			<Header />
 
-export default App
+			<Market />
+
+			<Footer />
+		</div>
+	);
+};
+
+export default App;
