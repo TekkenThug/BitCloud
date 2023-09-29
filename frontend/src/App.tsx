313a9d204e38c6140fac466c5124a92369f6ef89
Vadim Ignatov <ignatov0131@gmail.com>
2023-09-29 20:00:04 +0300

    feat: insert react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,31 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import Header from "@/components/common/header/Header";
-import Market from "@/components/pages/market/Market";
+import MarketPage from "@/components/pages/market/MarketPage";
+import AuthPage from "@/components/pages/auth/AuthPage";
 import Footer from "@/components/common/footer/Footer";
 
 import styles from "./App.module.scss";
 import "@/assets/styles/global.scss";
 
 const App = () => {
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			element: <MarketPage />,
+		},
+		{
+			path: "/auth",
+			element: <AuthPage />
+		}
+	]);
+
 	return (
 		<div className={styles.App}>
 			<Header />
 
 			<main className={styles.main}>
-				<Market />
+				<RouterProvider router={router} />
 			</main>
 
 			<Footer />
