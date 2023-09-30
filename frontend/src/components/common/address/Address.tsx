30a759b432428a0839a48e908ed63544c44ba073
Vadim Ignatov <ignatov0131@gmail.com>
2023-09-30 17:44:49 +0300

    feat: create address component

---
+++ frontend/src/components/common/address/Address.tsx
@@ -0,0 +1,13 @@
+import styles from "./Address.module.scss";
+
+import LockIcon from "@/assets/icons/ui/lock.svg?react";
+
+const Address = () => {
+	return (
+		<div className={styles.Address}>
+			<LockIcon className={styles.icon} /><span className={styles.green}>https://</span>accounts.bitcloud.com/login
+		</div>
+	);
+};
+
+export default Address;
