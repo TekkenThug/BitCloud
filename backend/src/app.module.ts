e3a792a7fc0e6a14090d32080caa9cfbc74af8a6
Vadim Ignatov <ignatov0131@gmail.com>
2023-10-14 18:41:12 +0300

    feat: create currency module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CurrencyModule } from './currency/currency.module';
 
 @Module({
-  imports: [],
+  imports: [CurrencyModule],
   controllers: [AppController],
   providers: [AppService],
 })
