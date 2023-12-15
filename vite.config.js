9ffdc4ebaa3c6f0ca3abad902729fe11586c7002
Vadim Ignatov <ignatov0131@gmail.com>
2023-12-15 18:37:49 +0300

    chore: replace frontend files

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,26 @@
-import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import svgr from "vite-plugin-svgr";
 
+// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/frontend/main.tsx'],
             refresh: true,
         }),
+        react(),
+        tsconfigPaths(),
+        svgr(),
     ],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: "@use './src/assets/styles/exported' as *;"
+            }
+        }
+    },
 });
+
