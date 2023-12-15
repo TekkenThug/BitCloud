4d46b62e0b1b91dfbf268aff8eb7f3718994ab7f
Vadim Ignatov <ignatov0131@gmail.com>
2023-12-15 18:07:09 +0300

    feat: delete nodejs backend

--- backend/src/currency/currency.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CurrencyController } from './currency.controller';
-import { CurrencyService } from './currency.service';
-
-@Module({
-  controllers: [CurrencyController],
-  providers: [CurrencyService],
-})
-export class CurrencyModule {}
