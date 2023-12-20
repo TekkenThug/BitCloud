7a8a4febf263ad58a2e4fbace1762f9ac38f8237
Vadim Ignatov <ignatov0131@gmail.com>
2023-12-20 17:36:41 +0300

    fix: restore fonts

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,300;9..40,400;9..40,500;9..40,600;9..40,700&family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
     <title>Crypto exchange</title>
 </head>
 
