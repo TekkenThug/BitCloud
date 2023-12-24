be023177c60f499d7700c379f328a79942642498
Vadim Ignatov <ignatov0131@gmail.com>
2023-12-24 16:52:16 +0300

    refactor: change service to article resource

---
+++ app/Http/Resources/ArticleResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ArticleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'author' => $this->user->first_name . " " . $this->user->last_name,
+            'id' => $this->id,
+            'date' => $this->created_at,
+            'tag' => $this->articleTag->label,
+            'cover_url' => $this->cover_url,
+            'title' => $this->title,
+        ];
+    }
+}
