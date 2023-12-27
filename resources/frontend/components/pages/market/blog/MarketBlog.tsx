b221825311f87b228685a62b73b19593192a4200
Vadim Ignatov <ignatov0131@gmail.com>
2023-12-27 11:51:03 +0300

    chore: test commit

--- resources/frontend/components/pages/market/blog/MarketBlog.tsx
+++ resources/frontend/components/pages/market/blog/MarketBlog.tsx
@@ -1,88 +1,95 @@
-import MarketBlogItem, { BlogItem } from "./item/MarketBlogItem.tsx";
+import { useEffect, useState } from "react";
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
+
+import { getArticlesForMarketPage } from "@/services/api/contexts/article";
+
+import MarketBlogItem from "./item/MarketBlogItem.tsx";
 import UiButton from "@/components/ui/button/UiButton.tsx";
 
 import styles from "./MarketBlog.module.scss";
 import Loading from "@/assets/icons/controls/loading.svg?react";
 
 const MarketBlog = () => {
-	const title = "Learn and earn";
-	const subtitle = "Stacks is a production-ready library of stackable content blocks built in React Native.";
-	const blogList: BlogItem[] = [
-		{
-			title: "Learn about UI8 coin and earn an All-Access Pass",
-			author: "Floyd Buckridge",
-			date: "Feb 03, 2021",
-			tags: [
-				{
-					title: "New",
-					color: "red"
-				}
-			]
-		},
-		{
-			title: "Submit your watchlist and win USDT",
-			author: "Floyd Buckridge",
-			date: "Feb 03, 2021",
-			tags: [
-				{
-					title: "Featured",
-					color: "blue"
-				}
-			]
-		},
-		{
-			title: "Submit your",
-			author: "Floyd Buckridge",
-			date: "Feb 03, 2021",
-			tags: [
-				{
-					title: "Featured",
-					color: "blue"
-				}
-			]
-		},
-	];
+    const title = "Learn and earn";
+    const subtitle = "Stacks is a production-ready library of stackable content blocks built in React Native.";
 
-	const handle = () => alert("hello");
+    const [enabled, setEnabled] = useState(true);
+    useEffect(() => {
+        setEnabled(false);
+    }, []);
 
-	return (
-		<section className={styles.MarketBlog}>
-			<div className="container">
-				<div className={styles.content}>
-					<h2 className={styles.title}>
-						{ title }
-					</h2>
+    const [
+        page,
+        setPage
+    ] = useState<number>(1);
+    const { data, isLoading, refetch } = useQuery({
+        queryKey: ["blogArticles", page],
+        queryFn: () => getArticlesForMarketPage(page),
+        enabled: enabled,
+    });
 
-					<p className={styles.subtitle}>
-						{ subtitle }
-					</p>
-				</div>
+    const [articles, setArticles] = useState([]);
+    useEffect(() => {
+        if (!data || !data.articles.length) return;
 
-				<ul className={styles.blogList}>
-					{
-						blogList && blogList.map((item, index) => (
-							<MarketBlogItem
-								key={index}
-								className={styles.blogItem}
-								title={item.title}
-								author={item.author}
-								date={item.date}
-								tags={item.tags}
-							/>
-						))
-					}
-				</ul>
+        console.log(data.articles);
 
-				<UiButton
-					className={styles.loadingButton}
-					color="dark"
-					clickHandler={handle}
-				>
-					<Loading className={styles.loadingButtonIcon} /> Load more
-				</UiButton>
-			</div>
-		</section>
-	);
+        setArticles(value => [...value, ...data.articles]);
+    }, [data]);
+
+    const loadMoreArticles = () => {
+        if (!data || data.pagination.lastPage === page) return;
+
+        setPage((page) => page + 1);
+        refetch();
+    };
+
+    return (
+        <section className={styles.MarketBlog}>
+            <div className="container">
+                <div className={styles.content}>
+                    <h2 className={styles.title}>
+                        {title}
+                    </h2>
+
+                    <p className={styles.subtitle}>
+                        {subtitle}
+                    </p>
+                </div>
+
+                {
+                    !isLoading &&
+                    <ul className={styles.blogList}>
+                        {
+                            data && articles.map((item, index) => (
+                                <MarketBlogItem
+                                    key={index}
+                                    className={styles.blogItem}
+                                    title={item.title}
+                                    author={item.author}
+                                    date={item.date}
+                                    cover={item.coverUrl}
+                                    tag={item.tag}
+                                />
+                            ))
+                        }
+                    </ul>
+                }
+
+                {
+                    data?.pagination.lastPage !== page &&
+                    <UiButton
+                        className={styles.loadingButton}
+                        color="dark"
+                        clickHandler={loadMoreArticles}
+                        disabled={isLoading}
+                    >
+                        <Loading className={`${styles.loadingButtonIcon} ${isLoading && "spin-animation"}`}/> Load more
+                    </UiButton>
+                }
+            </div>
+        </section>
+    );
 };
 
 export default MarketBlog;
