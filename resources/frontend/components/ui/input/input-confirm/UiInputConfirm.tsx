5d9b1eb0141f37d76d30398cd6915e182c8c750c
Vadim Ignatov <ignatov0131@gmail.com>
2023-12-30 16:44:13 +0300

    feat: add error state for input

--- resources/frontend/components/ui/input/input-confirm/UiInputConfirm.tsx
+++ resources/frontend/components/ui/input/input-confirm/UiInputConfirm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import{ useState, FC, FormEvent } from "react";
 
 import "./UiInputConfirm.scss";
 
@@ -10,30 +10,42 @@ interface UiInputProps {
 	type?: string;
 	placeholder?: string;
 	submitHandle: (value: string) => void;
+    inputHandle?: () => void;
 	isLoading?: boolean;
 	isSuccess?: boolean;
+    isError?: boolean;
+    errorText?: string;
 }
 
 // TODO: make validation and incorrect state
-const UiInputConfirm: React.FC<UiInputProps> = (
+const UiInputConfirm: FC<UiInputProps> = (
     {
         type = "text",
         placeholder = "",
         isLoading = false,
         isSuccess = false,
-        submitHandle
+        isError = false,
+        errorText = "",
+        submitHandle,
+        inputHandle = () => {},
     }
 ) => {
     const [value, setValue] = useState("");
 
-    const handleInputValue = (event: React.FormEvent<HTMLInputElement>) => {
+    const handleInputValue = (event: FormEvent<HTMLInputElement>) => {
+        inputHandle();
         setValue(event.currentTarget.value);
     };
 
+    const classes = [
+        "UiInputConfirm",
+        isLoading ? "UiInputConfirm_loading" : "",
+        isSuccess ? "UiInputConfirm_success" : "",
+        isError ? "UiInputConfirm_error" : "",
+    ];
+
     return (
-        <div
-            className={`UiInputConfirm ${isLoading ? "UiInputConfirm_loading" : ""} ${isSuccess ? "UiInputConfirm_success" : ""}`}
-        >
+        <div className={classes.join(" ")}>
             <input
                 className="UiInputConfirm__nativeInput"
                 type={type}
@@ -43,15 +55,22 @@ const UiInputConfirm: React.FC<UiInputProps> = (
             />
 
             <button
-                disabled={!value}
+                disabled={!value || isError}
                 className="UiInputConfirm__extra-button"
                 onClick={() => submitHandle(value)}
             >
                 {
                     isLoading ? <Loading className="UiInputConfirm__icon spin-animation" /> :
-                        isSuccess ? <Check className="UiInputConfirm__icon"  /> : <ArrowRight className="UiInputConfirm__icon" />
+                        isSuccess ? <Check className="UiInputConfirm__icon"  /> :
+                            <ArrowRight className="UiInputConfirm__icon" />
                 }
             </button>
+
+            { isError && Boolean(errorText) &&
+                <p className="UiInputConfirm__error">
+                    { errorText }
+                </p>
+            }
         </div>
     );
 };
