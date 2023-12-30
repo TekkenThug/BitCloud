4bb04d0bea4120cc84153dfbafe04b6f9529d1bc
Vadim Ignatov <ignatov0131@gmail.com>
2023-12-30 18:42:24 +0300

    chore: change useEffect eslint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,7 @@ module.exports = {
             "warn",
             { allowConstantExport: true },
         ],
+        "react-hooks/exhaustive-deps": ["off"],
         "semi": "error",
         "indent": [
             "error",
