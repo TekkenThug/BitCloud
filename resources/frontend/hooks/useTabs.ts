13d31af9efb1891d4c912ecf8ec5b80d0da4f151
Vadim Ignatov <ignatov0131@gmail.com>
2024-01-01 13:36:15 +0300

    feat: create useTabs hook

---
+++ resources/frontend/hooks/useTabs.ts
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+const useTabs = (tabs: string[], activeTab?: string) => {
+    const [_tabs] = useState(tabs);
+    const [_activeTab, _setActiveTab] = useState(activeTab || _tabs[0]);
+
+    return {
+        tabs: _tabs,
+        activeTab: _activeTab,
+        setTab: _setActiveTab
+    };
+};
+
+export default useTabs;
