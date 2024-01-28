0d539e0bbef6f70cc6ea7610035d709b6c6cd726
Vadim Ignatov <ignatov0131@gmail.com>
2024-01-28 17:52:48 +0300

    feat(middleware): create only dev middleware

---
+++ app/Http/Middleware/AcceptDevEnv.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AcceptDevEnv
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ('local' !== app()->environment()) {
+            abort(403, 'You are not authorized to access this');
+        }
+
+        return $next($request);
+    }
+}
