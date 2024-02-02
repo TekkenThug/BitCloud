93a41acda830bfd3019133d2034369ab3586e044
Vadim Ignatov <ignatov0131@gmail.com>
2024-02-02 10:16:44 +0300

    feat(LoginController): create

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Resources\Users\UserResource;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class LoginController extends Controller
+{
+    public function login(LoginRequest $request): \Illuminate\Http\JsonResponse
+    {
+        $credentials = $request->validated();
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return response()->json([
+                'data' => new UserResource(Auth::user()),
+                'message' => 'Successfully login'
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Incorrect credentials'
+        ], Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function logout(Request $request): \Illuminate\Http\JsonResponse
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return response()->json([
+            'message' => 'Successfully logout'
+        ]);
+    }
+}
