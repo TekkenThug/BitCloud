e2a626fb595a0324df9c4d85157163a2ae030dde
Vadim Ignatov <ignatov0131@gmail.com>
2024-02-02 11:30:53 +0300

    feat(data): create http codes

---
+++ resources/frontend/data/httpCode.ts
@@ -0,0 +1,13 @@
+const HTTP_CODE = {
+    OK: 200,
+    NO_CONTENT: 204,
+
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+
+    INTERNAL_SERVER_ERROR: 500,
+    BAD_GATEWAY: 502,
+};
+
+export default HTTP_CODE;
