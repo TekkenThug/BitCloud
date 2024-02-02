1fac6cf0b6fb039ceebb5a619da3ff05983ed648
Vadim Ignatov <ignatov0131@gmail.com>
2024-02-02 13:25:21 +0300

    feat(Address): provide variable and props

--- resources/frontend/components/common/address/Address.tsx
+++ resources/frontend/components/common/address/Address.tsx
@@ -1,12 +1,21 @@
+import { FC } from "react";
+
 import styles from "./Address.module.scss";
 
 import LockIcon from "@/assets/icons/ui/lock.svg?react";
 
-const Address = () => {
+const [protocol, host] = import.meta.env.VITE_APP_URL.split("://");
+
+interface Props {
+    address?: string
+}
+
+const Address: FC<Props> = ({ address = "/login" }) => {
     return (
         <div className={ styles.Address }>
             <LockIcon className={ styles.icon } />
-            <span className={ styles.green }>https://</span>accounts.bitcloud.com/login
+
+            <span className={ styles.green }>{ protocol }://</span>{ host }{ address }
         </div>
     );
 };
