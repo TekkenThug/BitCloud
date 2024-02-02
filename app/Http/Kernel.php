2f52dd5e5c3412e2712a38a8323ef50f971a9378
Vadim Ignatov <ignatov0131@gmail.com>
2024-02-02 10:19:32 +0300

    feat(Kernel): add sanctum middlewares

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,7 +39,8 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            // \Illuminate\Session\Middleware\StartSession::class, # Middleware for testing on Postman
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
