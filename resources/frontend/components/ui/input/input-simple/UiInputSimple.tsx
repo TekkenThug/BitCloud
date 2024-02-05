e47fc992f82418145b009c0db5d6ee33d46199b8
Vadim Ignatov <ignatov0131@gmail.com>
2024-02-05 12:40:42 +0300

    feat(UiInputSimple): create password button

--- resources/frontend/components/ui/input/input-simple/UiInputSimple.tsx
+++ resources/frontend/components/ui/input/input-simple/UiInputSimple.tsx
@@ -1,23 +1,45 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
 import "./UiInputSimple.scss";
 
-interface UiInputSimpleProps {
+import Eye from "@/assets/icons/ui/eye.svg?react";
+
+interface Props {
 	type?: string;
 	label?: string;
 	placeholder?: string;
 	className?: string;
+    hasShowPassword?: boolean;
+    onChange: (value: string) => void;
 }
 
-const UiInputSimple: FC<UiInputSimpleProps> = (
+const UiInputSimple: FC<Props> = (
     {
         type = "text",
         label = "",
         placeholder = "",
         className = "",
+        hasShowPassword = false,
+        onChange,
     }
 ) => {
-    const inputField = <input className="UiInputSimple__nativeInput" type={ type } placeholder={ placeholder } />;
+    const [showPassword, setShowPassword] = useState(false);
+
+    const inputField =
+        <div className="UiInputSimple__inputWrapper">
+            <input
+                className="UiInputSimple__nativeInput"
+                type={ type === "password" ? showPassword ? "text" : "password" : type }
+                placeholder={ placeholder }
+                onChange={ ({ target }) => onChange(target.value) }
+            />
+
+            { hasShowPassword &&
+                <button className="UiInputSimple__showPasswordButton" onClick={ () => setShowPassword(!showPassword) }>
+                    <Eye />
+                </button>
+            }
+        </div>;
     const inputFieldWithLabel = <label className="UiInputSimple__label"><span>{ label }</span>{ inputField }</label>;
 
     return (
