a3f3af4f1794f083100e740847819e6d7e9f5055
Vadim Ignatov <ignatov0131@gmail.com>
2024-02-06 15:54:26 +0300

    feat(RegisterRequest): create validated request

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,11 @@ class AppServiceProvider extends ServiceProvider
             return Http::withHeaders([
                 'X-CMC_PRO_API_KEY' => config("api.market_api_key"),
             ])->baseUrl(config("api.market_api_url"));
+        });
+
+        // Default password rule
+        Password::defaults(function () {
+            return Password::min(6)->mixedCase()->numbers()->symbols();
         });
     }
 }
