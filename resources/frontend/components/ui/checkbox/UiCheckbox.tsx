577f5e3c9775605983b3238deb80848a6898ccc7
Vadim Ignatov <ignatov0131@gmail.com>
2024-02-07 11:14:07 +0300

    feat(UiCheckbox): create component

---
+++ resources/frontend/components/ui/checkbox/UiCheckbox.tsx
@@ -0,0 +1,52 @@
+import { forwardRef, ReactNode } from "react";
+import { UseFormRegister } from "react-hook-form";
+import classNames from "classnames";
+
+import "./UiCheckbox.scss";
+
+import Check from "@/assets/icons/ui/check.svg?react";
+
+interface Props {
+    children?: ReactNode
+    error?: boolean
+}
+
+const UiCheckbox = forwardRef<HTMLInputElement, Props & ReturnType<UseFormRegister<never>>>((
+    {
+        children = null,
+        error = false,
+        onChange,
+        onBlur,
+        name,
+    },
+    ref
+) => {
+    const classes = classNames("UiCheckbox", { "UiCheckbox--error": error });
+
+    return (
+        <label className={ classes }>
+            <input
+                ref={ ref }
+                type="checkbox"
+                className="UiCheckbox__checkbox"
+                onChange={ onChange }
+                onBlur={ onBlur }
+                name={ name }
+            />
+
+            <span className="UiCheckbox__fakeCheckbox">
+                <Check />
+            </span>
+
+            { Boolean(children) &&
+                <div>
+                    { children }
+                </div>
+            }
+        </label>
+    );
+});
+
+UiCheckbox.displayName = "UiCheckbox";
+
+export default UiCheckbox;
