d14ca23ec990922d79cbe7b41b3760b66f692509
Vadim Ignatov <ignatov0131@gmail.com>
2024-02-07 11:13:16 +0300

    feat(validations): create register form schema

--- resources/frontend/services/validations/schemas/auth.ts
+++ resources/frontend/services/validations/schemas/auth.ts
@@ -1,6 +1,18 @@
 import { z } from "zod";
 
+import { PASSWORD_REGEXP } from "@/data/regExps.ts";
+
 export const loginForm = z.object({
     email: z.string().min( 1, "Email is required").email(),
     password: z.string().min(1, "Password is required")
+});
+
+export const registerForm = z.object({
+    email: z.string().min( 1, "Email is required").email(),
+    password: z.string().min(1, "Password is required").regex(PASSWORD_REGEXP, "Password must"),
+    confirmPassword: z.string().min(1, "Password is required"),
+    agreement: z.literal(true)
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
 });
