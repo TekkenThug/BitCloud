238748d581921e6de0c8c3bebcd55723539beacd
Vadim Ignatov <ignatov0131@gmail.com>
2024-02-08 18:10:28 +0300

    fix(eslint): lint

--- resources/frontend/main.tsx
+++ resources/frontend/main.tsx
@@ -3,8 +3,9 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import App from "./App";
 import { store } from "@/store";
+
+import App from "./App";
 
 const queryClient = new QueryClient();
 
