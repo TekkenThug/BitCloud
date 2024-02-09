2e04f6957de5172909c9c5f08913ebc3ce24333f
Vadim Ignatov <ignatov0131@gmail.com>
2024-02-09 09:49:03 +0300

    test

--- resources/frontend/main.tsx
+++ resources/frontend/main.tsx
@@ -1,20 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { store } from "@/store";
 
 import App from "./App";
 
-const queryClient = new QueryClient();
-
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <Provider store={ store }>
-            <QueryClientProvider client={ queryClient }>
-                <App />
-            </QueryClientProvider>
+            <App />
         </Provider>
     </StrictMode>,
 );
