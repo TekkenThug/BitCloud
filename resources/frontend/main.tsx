7904bffcf069da2cb55887045cc9faca658ae5c8
Vadim Ignatov <ignatov0131@gmail.com>
2024-02-09 10:27:02 +0300

    test: husky

--- resources/frontend/main.tsx
+++ resources/frontend/main.tsx
@@ -1,15 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { store } from "@/store";
 
 import App from "./App";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <Provider store={ store }>
-            <App />
+            <QueryClientProvider client={ queryClient }>
+                <App />
+            </QueryClientProvider>
         </Provider>
     </StrictMode>,
 );
