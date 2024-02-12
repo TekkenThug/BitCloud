7180dbc0f8e9971deb819605f67b4f239be38f69
Vadim Ignatov <ignatov0131@gmail.com>
2024-02-12 19:05:23 +0300

    feat(storybook): create label story

---
+++ resources/frontend/storybook/stories/UiLabel.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import UiLabel from "@/components/ui/label/UiLabel.tsx";
+
+const meta: Meta<typeof UiLabel> = {
+    title: "BitCloud/UI/Label",
+    component: UiLabel,
+    parameters: {
+        layout: "centered",
+    },
+    tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof UiLabel>;
+
+export const Default: Story = {
+    args: {
+        title: "Label",
+        color: "blue",
+    },
+};
