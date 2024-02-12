b4b695db1801f169c7409328c4b5afb30df9bafc
Vadim Ignatov <ignatov0131@gmail.com>
2024-02-12 19:03:40 +0300

    feat(storybook): create button story

---
+++ resources/frontend/storybook/stories/UiButton.stories.ts
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import UiButton from "@/components/ui/button/UiButton.tsx";
+
+const meta: Meta<typeof UiButton> = {
+    title: "BitCloud/UI/Button",
+    component: UiButton,
+    parameters: {
+        layout: "centered",
+    },
+    tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof UiButton>;
+
+export const Default: Story = {
+    args: {
+        color: "blue",
+        size: "medium",
+        children: "Button",
+    },
+};
