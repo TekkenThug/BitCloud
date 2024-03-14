6aceb0a10fc1601ddea046824c5850a4e54e43d6
Vadim Ignatov <ignatov0131@gmail.com>
2024-03-14 20:26:21 +0300

    refactor(CurrencyController): remove service container

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -6,25 +6,13 @@ use App\Services\CurrencyService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
-/**
- * @group Currencies
- */
 class CurrencyController extends Controller
 {
-    private CurrencyService $currencyService;
-
-    public function __construct()
-    {
-        $this->currencyService = new CurrencyService();
-    }
-
     /**
      * Get currencies
-     *
-     * @authenticated
      */
-    public function get(Request $request): JsonResponse
+    public function get(CurrencyService $currencyService, Request $request): JsonResponse
     {
-        return response()->json(['data' => $this->currencyService->getLatestTrends($request->query('limit', 100))]);
+        return response()->json(['data' => $currencyService->getLatestTrends($request->query('limit', 100))]);
     }
 }
