81c5f6c55149078b862ff26de5e6d43df505d253
Vadim Ignatov <ignatov0131@gmail.com>
2024-03-19 14:49:23 +0300

    feat: test commit

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -20,32 +20,32 @@ class ArticleController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-//    public function store(Request $request)
-//    {
-//        //
-//    }
+    //    public function store(Request $request)
+    //    {
+    //        //
+    //    }
 
     /**
      * Display the specified resource.
      */
-//    public function show(string $id)
-//    {
-//        //
-//    }
+    //    public function show(string $id)
+    //    {
+    //        //
+    //    }
 
     /**
      * Update the specified resource in storage.
      */
-//    public function update(Request $request, string $id)
-//    {
-//        //
-//    }
+    //    public function update(Request $request, string $id)
+    //    {
+    //        //
+    //    }
 
     /**
      * Remove the specified resource from storage.
      */
-//    public function destroy(string $id)
-//    {
-//        //
-//    }
+    //    public function destroy(string $id)
+    //    {
+    //        //
+    //    }
 }
