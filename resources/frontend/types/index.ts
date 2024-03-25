b941379fc1cf84ac39246f50ad3c7cd9b7f26553
Vadim Ignatov <ignatov0131@gmail.com>
2024-03-25 17:01:27 +0300

    feat: make breadcrumbs interface

--- resources/frontend/types/index.ts
+++ resources/frontend/types/index.ts
@@ -1,5 +1,10 @@
 import { Currency } from "@/services/api/data-contracts.ts";
 
+export interface Breadcrumb {
+    link: string;
+    text: string;
+}
+
 export interface QuotatedCurrency extends Omit<Currency, "quote"> {
     quote: { x: string; y: number }[];
     number: number;
